69e9260ae9dc04cf7d05ed0858fea8957ff77af3
Student <example@example.com>
2019-10-31 10:37:51 -0700

    complete project, evaluates all numbers up to 3999. Throws error if user inputs a number of 4000 or more

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 var numerals = ["I", "V", "X", "L", "C", "D", "M"];
 var stringArray = [];
 
-// function romanize (number) {
-//
-//   ones(number);
-//   tens(number);
-//   hundreds(number);
-//   thousands(number);
-//
-//   return stringArray.join();
-// }
+function romanize (array) {
+  if (array[3] >= 4){
+    return "You done messed up A-A-Ron."
+  }
+  else{
+    thousands(array[3]);
+    hundreds(array[2]);
+    tens(array[1]);
+    ones(array[0]);
+
+    return stringArray.join("");
+  }
+}
+
 
 function ones (number) {
   if(number <=3){
@@ -47,11 +52,8 @@ function tens (number){
       }
   } else if (number === 9){
       stringArray.push(numerals[2] + numerals[4]);
-  }else{console.log("no tens")}
-  };
-
-
-
+  } else{console.log("no tens")}
+};
 
 
 function hundreds (number) {
@@ -70,11 +72,9 @@ function hundreds (number) {
       }
   } else if (number === 9){
       stringArray.push(numerals[4] + numerals[6]);
-  }else {console.log("no hundreds");}
-
-
-
+  } else {console.log("no hundreds");}
 };
+
 
 function thousands (number) {
 
@@ -83,31 +83,25 @@ function thousands (number) {
       stringArray.push(numerals[6]);
     }
   } else {console.log("You cannot count higher than 4000 in Roman Numerals!");}
+};
 
 
-
-
-
-
-}
 
 
 //front end
 $(document).ready(function(){
   $("#userInputForm").submit(function(event){
     event.preventDefault();
+    $(".outputBox").hide();
+    stringArray = [];
 
     var someVar = $("#userInput").val();
-
     var arr = someVar.split('').map(Number);
     var reversedArr = arr.reverse();
 
 
-    // romanize(reversedArr);
-
-    thousands(3);
-    console.log(stringArray);
-
+    $("#outputText").text(romanize(reversedArr));
+    $(".outputBox").slideDown();
 
   });
 });
